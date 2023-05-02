b8ac2f77e7136f8d2bc884aec9918f1a651cc56a
Alex Kostenko <0984181760ss@gmail.com>
2023-05-02 15:20:59 +0300

    fixing styles right block

--- src/components/RightBlock/style.ts
+++ src/components/RightBlock/style.ts
@@ -8,6 +8,7 @@ export const RootRightBlock = styled.div`
   height: 100%;
   background: ${PALETTE.lightPurple};
   display: flex;
+  justify-content: center;
 
   @media (max-width: 1450px) {
     width: 55%;
